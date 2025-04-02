149e163a596a767c13f76bf41e03abbe63b19a14
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-04-02 16:34:18 +0200

    Implement INSERT with DQL in contract repo

--- src/main/java/org/dcistudent/employeemanagementsystem/repositories/ContractRepository.java
+++ src/main/java/org/dcistudent/employeemanagementsystem/repositories/ContractRepository.java
@@ -1,9 +1,26 @@
 package org.dcistudent.employeemanagementsystem.repositories;
 
+import org.dcistudent.employeemanagementsystem.entities.Client;
 import org.dcistudent.employeemanagementsystem.entities.Contract;
+import org.dcistudent.employeemanagementsystem.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 
 @Repository
 public interface ContractRepository extends JpaRepository<Contract, Long> {
+  @Modifying
+  @Query(
+      "INSERT INTO Contract (name, dateStart, dateEnd, employee, client) " +
+      "VALUES (:name, :dateStart, :dateEnd, :employee, :client)")
+  void create(
+      String name,
+      LocalDate dateStart,
+      LocalDate dateEnd,
+      Employee employee,
+      Client client
+  );
 }
