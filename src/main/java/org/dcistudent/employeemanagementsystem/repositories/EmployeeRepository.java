db50834536623fa96e91e2293201ef13ae00be97
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-04-02 15:53:51 +0200

    Implement INSERT with DQL in employee repo

--- src/main/java/org/dcistudent/employeemanagementsystem/repositories/EmployeeRepository.java
+++ src/main/java/org/dcistudent/employeemanagementsystem/repositories/EmployeeRepository.java
@@ -2,8 +2,27 @@ package org.dcistudent.employeemanagementsystem.repositories;
 
 import org.dcistudent.employeemanagementsystem.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+  @Modifying
+  @Query(
+      "INSERT INTO Employee (firstName, lastName, email, phone, dateHire, salary, department) " +
+      "VALUES (:firstName, :lastName, :email, :phone, :dateHire, :salary, :departmentId)"
+  )
+  void create(
+      String firstName,
+      String lastName,
+      String email,
+      String phone,
+      LocalDate hireDate,
+      BigDecimal salary,
+      Long departmentId
+  );
 }
