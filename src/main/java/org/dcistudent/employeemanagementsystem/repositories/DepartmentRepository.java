2aade473a95eeb356ec4c5c9ba38a5fa265f9e0c
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-04-02 15:53:40 +0200

    Implement INSERT with DQL in department repo

--- src/main/java/org/dcistudent/employeemanagementsystem/repositories/DepartmentRepository.java
+++ src/main/java/org/dcistudent/employeemanagementsystem/repositories/DepartmentRepository.java
@@ -2,8 +2,16 @@ package org.dcistudent.employeemanagementsystem.repositories;
 
 import org.dcistudent.employeemanagementsystem.entities.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+  @Modifying
+  @Query(
+      "INSERT INTO Department (name, location) VALUES (:name, :location)"
+  )
+  void create(@Param("name") String name, @Param("location") String location);
 }
