2bfbffe509775041b5aba050e319b5576b79cc6f
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-04-02 16:18:17 +0200

    Implement INSERT with DQL in client repo

--- src/main/java/org/dcistudent/employeemanagementsystem/repositories/ClientRepository.java
+++ src/main/java/org/dcistudent/employeemanagementsystem/repositories/ClientRepository.java
@@ -2,8 +2,15 @@ package org.dcistudent.employeemanagementsystem.repositories;
 
 import org.dcistudent.employeemanagementsystem.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+  @Modifying
+  @Query(
+      "INSERT INTO Client (name, industry, email, phone) VALUES (:name, :industry, :email, :phoneNumber)"
+  )
+  void create(String name, String industry, String email, String phoneNumber);
 }
